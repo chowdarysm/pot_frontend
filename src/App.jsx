63651ff8a750232c1679ade3c0fa7cb590b781e9
ParthivM1 <119024676+ParthivM1@users.noreply.github.com>
2025-08-04 13:18:14 -0400

    fixed app sourcing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login/Login";
 import Dashboard from "./Dashboard/Dashboard";
 import Detailed from "./DetailedDash/Detailed";
-import Issues from "./Issues/Issues";
-import IssueProfile from "./IssueProfile/IssueProfile";
 import Report from "./Report/Report";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReportForm from "./ReportForm/ReportForm";
 import Signup from "./Signup/Signup";
 import Signin from "./Signin/Signin";
 import Billboards from "./Billboards/Billboards";
 import PotholeReport from "./PotholeReport/PotholeReport";
 import BillboardReport from "./BillboardReport/BillboardReport";
-import { ToastContainer, toast } from "react-toastify";
 import Potholes from "./Potholes/Potholes";
 import Events from "./Events/Events";
 import Videos from "./Videos/Videos";
@@ -25,6 +21,8 @@ import Images from "./Images/Images";
 import ReportDetails from "./ReportDetails/ReportDetails";
 import DetailedReport from "./DetailedReport/DetailedReport";
 import Navbar from "./Navbar/Navbar";
+import { ToastContainer } from "react-toastify";
+
 function App() {
   return (
     <div className="App">
@@ -36,9 +34,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/detailed" element={<Detailed />} />
           <Route path="/report" element={<Report />} />
-          <Route path="/detailed" element={<Detailed />} />
           <Route path="/form" element={<ReportForm />} />
-
           <Route path="/billboards" element={<Billboards />} />
           <Route path="/potholereport" element={<PotholeReport />} />
           <Route path="/billboardreport" element={<BillboardReport />} />
@@ -47,14 +43,14 @@ function App() {
           <Route path="/videos" element={<Videos />} />
           <Route path="/home" element={<Home />} />
           <Route path="/videoreport" element={<VideoReport />} />
-          <Route path="/videodetails" element={<VideoDetail />} />
+          {/* Corrected Route Path */}
+          <Route path="/video/:guid" element={<VideoDetail />} />
           <Route path="/imagedetails" element={<ImageDetail />} />
           <Route path="/images" element={<Images />} />
           <Route path="/reportdetails" element={<ReportDetails />} />
           <Route path="/detailedreport" element={<DetailedReport />} />
         </Route>
       </Routes>
-
       <ToastContainer />
     </div>
   );
