95649f68bd39878f8e9bb220e52c84939c7c23a6
unknown <sunilchm@gmail.com>
2025-08-12 16:20:40 -0400

    modified event page

--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 import "./Events.css";
 import { useNavigate } from "react-router-dom";
-const [reportData, setReportData] = useState([]); // Initialize with an empty array
-
-/*get_total_processed_reports */
 const Events = () => {
   const navigate = useNavigate();
   const goToBack = () => {
     navigate(-1);
   };
-
- 
-
   return (
     <>
       <div className="event-container">
         <div className="back-btn">
           <button onClick={goToBack}>Back</button>
-        </div>
-    
+        </div>        
         <div className="video-container">
           <div className="process-video-data">
             <h1>Processed File Data</h1>
@@ -30,16 +23,15 @@ const Events = () => {
                 <tr>
                   <th>Last processed date</th>
                   <th>File name</th>
-                  <th>Location</th>
-                  
+                  <th>Location</th>                 
                 </tr>
               </thead>
-              <tbody>
+              <tbody>                 
                 <tr>
-                  <td>28-Jul-25</td>
+                  <td>27-Jul-25</td>
                   <td>NH14_2807202505100000.mp4</td>
-                  <td>236.687.867.901</td>                 
-                </tr> 
+                  <td>236.687.867.802</td>                 
+                </tr>
               </tbody>
             </table>
           </div>
