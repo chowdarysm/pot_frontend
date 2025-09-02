dd1aaa828ae46f468bdb7a67f04f5b20fbced216
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2025-09-02 16:21:30 +0530

    change

--- src/MapView/MapView.jsx
+++ src/MapView/MapView.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
-// const containerStyle = {
-//   width: "100%",
-//   height: "500px",
-// };
-
 const center = {
   lat: 18.5204,
   lng: 73.8567,
